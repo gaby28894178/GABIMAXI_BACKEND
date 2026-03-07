70a91b19ab1c727e9bfd14ef2b1d43a03235b786
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-03-06 22:48:16 -0300

    Implement dynamic DB switching (MySQL/Postgres) based on env vars

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,22 @@
-export { pool } from './db.pg.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Determine which DB to use
+// Default to MySQL if POSTGRES_URL is not set and DB_CLIENT is not 'pg'
+const shouldUsePostgres = process.env.POSTGRES_URL || process.env.DB_CLIENT === 'pg';
+
+let pool;
+
+if (shouldUsePostgres) {
+  // Use PostgreSQL
+  const { pool: pgPool } = await import('./db.pg.js');
+  pool = pgPool;
+  console.log('Using PostgreSQL configuration');
+} else {
+  // Use MySQL
+  const { pool: mysqlPool } = await import('./db.mysql.js');
+  pool = mysqlPool;
+  console.log('Using MySQL configuration');
+}
+
+export { pool };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,17 @@
-export { userModel } from './user.model.pg.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Determine which DB to use
+const shouldUsePostgres = process.env.POSTGRES_URL || process.env.DB_CLIENT === 'pg';
+
+let userModel;
+
+if (shouldUsePostgres) {
+  const { userModel: pgModel } = await import('./user.model.pg.js');
+  userModel = pgModel;
+} else {
+  const { userModel: mysqlModel } = await import('./user.model.mysql.js');
+  userModel = mysqlModel;
+}
+
+export { userModel };
